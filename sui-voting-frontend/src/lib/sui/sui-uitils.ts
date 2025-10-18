569af93fb3163db7f8989e17798c230acac1ad4c
unknown <emmanueladesipe01@gmail.com>
2025-10-18 17:15:15 +0100

    created Sui Utils file

---
+++ sui-voting-frontend/src/lib/sui/sui-uitils.ts
@@ -0,0 +1,7 @@
+import { Transaction } from "@mysten/sui/transactions";
+
+interace PollOptions {}
+
+export const mintPoll = (options: PollOptions): Transaction => {
+	const tx = new Transaction()
+}
